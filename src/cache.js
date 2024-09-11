c9a0ef40a62ca05789b02fc218f5c7408cf08182
alex <inrag3x@gmail.com>
2024-09-11 23:21:57 +0300

    add: implementation

--- src/cache.js
+++ src/cache.js
@@ -1,18 +1,33 @@
 class Cache {
     //[Key [Value, Hits]]
     constructor(values) {
+        this.values = values;
     }
 
     get count() {
+        return this.values.size;
     }
 
     call(key) {
+        const array = this.values.get(key);
+
+        if (array === undefined)
+            return null;
+
+        array[1] -= 1;
+
+        if (array[1] === 0)
+            this.values.delete(key);
+
+        return array[0];
     }
 
     hits(key) {
+        return this.values.get(key)[1];
     }
 
     set(key, value, hits= 1) {
+        this.values.set(key, [value, hits]);
     }
 
     statistics() {
